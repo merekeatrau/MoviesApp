56d4f33aedd6a725a3cd587d849e07c1d6d4c19c
mereke.atrauov <mereke.atrauov@nu.edu.kz>
2023-01-18 13:18:24 +0600

    Initial Commit

---
+++ MoviesAutoLayout/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MoviesAutoLayout
+//
+//  Created by Mereke on 18.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
